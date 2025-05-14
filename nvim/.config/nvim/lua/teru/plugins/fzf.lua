741a819f2cf4aabaae0961ea0dd3704abecb2eae
Teru <36055140+KhoTeru@users.noreply.github.com>
2025-05-13 23:27:53 -0500

    Replaced telescope.nvim with fzf-lua

---
+++ nvim/.config/nvim/lua/teru/plugins/fzf.lua
@@ -0,0 +1,19 @@
+return {
+        "ibhagwan/fzf-lua",
+        dependencies = {
+                "nvim-tree/nvim-web-devicons",
+                "OXY2DEV/markview.nvim"
+        },
+        config = function()
+
+                require("fzf-lua").setup({ files = { cwd_prompt = false }})
+                local builtin = require("fzf-lua")
+
+                vim.keymap.set("n", "<Leader>pf", builtin.files, {})
+                vim.keymap.set("n", "<Leader>pg", builtin.live_grep, {})
+                vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+                vim.keymap.set("n", "<Leader>pw", builtin.grep_cword, {})
+                vim.keymap.set("n", "<Leader>pW", builtin.grep_cWORD, {})
+                vim.keymap.set("n", "<Leader>vh", builtin.helptags, {})
+        end
+}
