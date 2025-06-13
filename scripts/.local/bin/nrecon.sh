9fa5c61755dd2a5c28d80b061fec16fc7ef4f2d3
Teru <36055140+KhoTeru@users.noreply.github.com>
2025-06-13 05:35:36 -0500

    Update nrecon.sh script

--- scripts/.local/bin/nrecon.sh
+++ scripts/.local/bin/nrecon.sh
@@ -23,7 +23,7 @@ echo -e "\n[+] Performing the nmap scan..."
 sudo nmap -p- --open -T4 --min-rate 10000 $(tg check) -n -Pn -oG tcpPorts -vvv
 
 echo -e "\n[+] Performing script scan..."
-nmap -p$(grep -oP '\d{1,5}/open' tcpPorts | sed 's#/open##' | paste -sd,) -sCV $(tg check) -oN targeted
+nmap -p$(grep -oP '\d{1,5}/open' tcpPorts | sed 's#/open##' | paste -sd,) -sCV $(tg check) -Pn -oN targeted
 
 echo -e "\n[+] Performing UDP scan..."
 sudo nmap --top-ports 100 -T4 --min-rate 10000 -sU $(tg check) -n -Pn -oN udpPorts -vvv
